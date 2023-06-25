48824e19a0b27fe85a8b6c0d1a9a4972d59b6c2b
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-06-25 17:43:45 +0500

    finshed

--- src/Ui/Dostafka.jsx
+++ src/Ui/Dostafka.jsx
@@ -1,64 +1,74 @@
 import imgDostafka from "../assets/images/BG.png";
-import Accordion from 'react-bootstrap/Accordion';
+import Accordion from "react-bootstrap/Accordion";
+import bike from "../assets/images/bike.png";
+import "./dostafka.scss";
 const Dostafka = () => {
-    const sectionStyle = {
-        background: `url(${imgDostafka})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-    };
+  const sectionStyle = {
+    background: `url(${imgDostafka})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  };
 
-    return (
-        <>
-            <section style={sectionStyle}>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8">
-                            <h2>Доставка по всему Ташкенту!</h2>
-                            <Accordion defaultActiveKey="0">
-                                <Accordion.Item eventKey="0">
-                                    <Accordion.Header>Accordion Item #1</Accordion.Header>
-                                    <Accordion.Body>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                        culpa qui officia deserunt mollit anim id est laborum.
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey="1">
-                                    <Accordion.Header>Accordion Item #2</Accordion.Header>
-                                    <Accordion.Body>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                        culpa qui officia deserunt mollit anim id est laborum.
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey="3">
-                                    <Accordion.Header>Accordion Item #3</Accordion.Header>
-                                    <Accordion.Body>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                                        culpa qui officia deserunt mollit anim id est laborum.
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                            </Accordion>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section style={sectionStyle}>
+        <div className="container">
+          <div className="row dostafka-sopid ">
+            <div className="col-lg-8 animate__animated animate__heartBeat">
+              <h2 className="dostafka-title text-white ">
+                Доставка по всему Ташкенту! 
+              </h2>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Accordion Item #1</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Accordion Item #2</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>Accordion Item #3</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+            <div className="col-lg-4 dostafka-img animate__animated animate__fadeInTopRight">
+              <img src={bike} alt="rasm" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Dostafka;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,29 +6,34 @@ import Txth1 from '../Txth1/Txth1';
 import Lorem from '../Lorem/Lorem';
 const Hero = () => {
     return (
-        <>
-            <section className='hero'>
-                <div className="container">
-                    <div className="row hero-row">
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <div className="hero-card">
-                                <Txth1 txt={"10% скидки на все бургеры!"} />
-                                <Lorem txt={`Общая сумма заказа должна быть больше или равна 70 000 сум. Предложение не распространяется на доставку. Акция "Товар дня" продлится до 31.12.2022. следите за предложениями!`} />
-                                <Btn txt={"В меню"} />
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-md-6 col-sm-12">
-                            <div className="hero-img" style={{ backgroundImage: `url(${heroImg})` }}>
-                                <div className="hero-img-text">
-                                    <Txth1 txt={"10%"} />
-                                    <Txth2 txt={"скидка"} />
-                                </div>
-                            </div>
-                        </div >
-                    </div>
+      <>
+        <section className="hero">
+          <div className="container">
+            <div className="row hero-row">
+              <div className="col-lg-6 col-md-6 col-sm-12">
+                <div className="hero-card animate__animated animate__fadeInLeft">
+                  <Txth1 txt={"10% скидки на все бургеры!"} />
+                  <Lorem
+                    txt={`Общая сумма заказа должна быть больше или равна 70 000 сум. Предложение не распространяется на доставку. Акция "Товар дня" продлится до 31.12.2022. следите за предложениями!`}
+                  />
+                  <Btn txt={"В меню"} />
                 </div>
-            </section>
-        </>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-12">
+                <div
+                  className="hero-img animate__animated animate__fadeInRight"
+                  style={{ backgroundImage: `url(${heroImg})` }}
+                >
+                  <div className="hero-img-text">
+                    <Txth1 txt={"10%"} />
+                    <Txth2 txt={"скидка"} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
     );
 };
 
